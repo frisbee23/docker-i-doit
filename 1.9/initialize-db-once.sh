0a9b98307986e0c79c6c2cf95f1d4b91ef4a1675
frisbee23 <frisbee23@gmail.com>
2017-09-27 18:04:57 +0200

    Update initialize-db-once.sh

--- 1.9/initialize-db-once.sh
+++ 1.9/initialize-db-once.sh
@@ -9,5 +9,5 @@ MYSQL_ROOT_PASSWORD=changeme
 docker exec -t idoit-mariadb sh -c "echo \"UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE User = 'root'; FLUSH PRIVILEGES;\" | mysql -p$MYSQL_ROOT_PASSWORD"
 
 # shutdown with innodb, to be able to move the files away, for idoit, performance
-#docker exec -t idoit-mariadb sh -c "mysql -uroot -p$MYSQL_ROOT_PASSWORD -e\"SET GLOBAL innodb_fast_shutdown = 0\""
-mv  /srv/mariadb-data/ib_logfile[01] /tmp
+docker exec -t idoit-mariadb sh -c "mysql -uroot -p$MYSQL_ROOT_PASSWORD -e\"SET GLOBAL innodb_fast_shutdown = 0\""
+
